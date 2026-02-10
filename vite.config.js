9e250d9c3efdd750906559b037732132633c1d77
kevinbalocos <kevinbalocos@gmail.com>
2026-02-10 17:08:04 +0800

    [fixed] white screen

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = process.env.VITE_BASE || "/";
+
 export default defineConfig({
-  base: '/portfolio-v1/',
+  base,
   plugins: [tailwindcss(), react()],
-    server: {
+  server: {
     proxy: {
       // proxy /api/* to your local backend
       "/api": {
         target: "http://localhost:4000",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
